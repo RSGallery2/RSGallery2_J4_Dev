9a6d93dd5ebb4704f1d0c35de675c4b8f0d2055c
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2023-10-03 21:13:50 +0200

    smal

--- .build/clean4release.bat
+++ .build/clean4release.bat
@@ -11,5 +11,7 @@ REM added to the release data
 REM del ..\admin\install.rsgallery2.php
 REM del ..\admin\rsgallery2.xml 
 
-ECHO ToDo: New actions
+REM ECHO ToDo: New actions
+
+ECHO NO Actions actually
 PROMPT
