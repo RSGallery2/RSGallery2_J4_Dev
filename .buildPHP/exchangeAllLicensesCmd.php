5ba315d0867e05723ae4f654e7185d50e1eabfd2
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2024-09-01 22:30:22 +0200

    start excageAllLicenses

---
+++ .buildPHP/exchangeAllLicensesCmd.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace exchangeAllLicenses;
+
+require_once "./commandLine.php";
+require_once "./exchangeAllLicenses.php";
+
+// use \DateTime;
+
+use function commandLine\argsAndOptions;
+use function commandLine\print_header;
+use function commandLine\print_end;
+
+
+$HELP_MSG = <<<EOT
+>>>
+class exchangeAllLicenses
+
+ToDo: option commands , example
+
+<<<
+EOT;
+
+
+/*================================================================================
+main (used from command line)
+================================================================================*/
+
+$optDefinition = "s:d:h12345";
+$isPrintArguments = false;
+
+list($inArgs, $options) = argsAndOptions($argv, $optDefinition, $isPrintArguments);
+
+$LeaveOut_01 = true;
+$LeaveOut_02 = true;
+$LeaveOut_03 = true;
+$LeaveOut_04 = true;
+$LeaveOut_05 = true;
+
+/*--------------------------------------------
+variables
+--------------------------------------------*/
+
+$srcFile = "";
+$dstFile = "";
+
+foreach ($options as $idx => $option)
+{
+	print ("idx: " . $idx . "\r\n");
+	print ("option: " . $option . "\r\n");
+
+	switch ($idx)
+	{
+		case 's':
+			$srcFile = $option;
+			break;
+
+		case 'd':
+			$dstFile = $option;
+			break;
+
+		case "h":
+			exit($HELP_MSG);
+
+		case "1":
+			$LeaveOut_01 = true;
+			print("LeaveOut_01");
+			break;
+		case "2":
+			$LeaveOut_02 = true;
+			print("LeaveOut__02");
+			break;
+		case "3":
+			$LeaveOut_03 = true;
+			print("LeaveOut__03");
+			break;
+		case "4":
+			$LeaveOut_04 = true;
+			print("LeaveOut__04");
+			break;
+		case "5":
+			$LeaveOut_05 = true;
+			print("LeaveOut__05");
+			break;
+
+		default:
+			print("Option not supported '" . $option . "'");
+			break;
+	}
+
+}
+
+//--- call function ---------------------------------
+
+// for start / end diff
+$start = print_header($options, $inArgs);
+
+$oExchangeAllLicenses = new exchangeAllLicenses($srcFile, $dstFile);
+
+$hasError = $oExchangeAllLicenses->funYYY();
+
+if ($hasError) {
+
+    print ("Error on function funYYY:" . $hasError);
+
+} else {
+
+    print ($oExchangeAllLicenses->text () . "\r\n");
+}
+
+
+print_end($start);
+
+print ("--- end  ---" . "\n");
+
