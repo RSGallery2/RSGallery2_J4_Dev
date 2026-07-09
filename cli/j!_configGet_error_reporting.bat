99b8b39bee74fb514f3107c48b63a28c89b2fbda
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-07-09 09:19:30 +0200

    RSG2 CLI commands

---
+++ cli/j!_configGet_error_reporting.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+REM j!_configGet_error_reporting.bat
+REM
+CLS
+
+REM Path for calling
+set ExePath=e:\wamp64\bin\php\php8.4.5\
+REM ECHO ExePath: "%ExePath%"
+
+if exist "%ExePath%php.exe" (
+    REM path known (WT)
+    ECHO ExePath: "%ExePath%"
+) else (
+    REM Direct call
+    ECHO PHP in path variable
+    set ExePath=
+)
+
+"%ExePath%php.exe" --version
+
+ECHO ----------------------------------------------
+ECHO.
+
+echo --- "%ExePath%php.exe" joomla.php config:get error_reporting
+"%ExePath%php.exe" joomla.php config:get error_reporting
+
+GOTO :EOF
+
+REM ------------------------------------------
+REM Adds given argument to the already known command arguments
+:AddNextArg
+    Set NextArg=%*
+    Set CmdArgs=%CmdArgs% %NextArg%
+    ECHO  '%NextArg%'
+GOTO :EOF
