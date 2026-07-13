68ae7f859cf98fb47f9b4511d0c2c67322474db7
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-07-13 11:08:59 +0200

    J! CLI commands

--- cli/j!_user.bat
+++ cli/j!_user.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
-REM j!_user.bat
 REM  show a joomgla user
 REM %1 user id
-REM
 CLS
 
 REM config id
@@ -12,34 +10,14 @@ if "%~1"=="" (
 	set usr_id=285
 )
 
-REM Path for calling
-set ExePath=e:\wamp64\bin\php\php8.4.5\
-REM ECHO ExePath: "%ExePath%"
-
-if exist "%ExePath%php.exe" (
-    REM path known (WT)
-    ECHO ExePath: "%ExePath%"
-) else (
-    REM Direct call
-    ECHO PHP in path variable
-    set ExePath=
-)
-
-"%ExePath%php.exe" --version
-
-ECHO ----------------------------------------------
+ECHO.
+ECHO --- php joomla.php user  --id=%usr_id% 
 ECHO.
 
-echo --- "%ExePath%php.exe" joomla.php user --id=%usr_id% 
-"%ExePath%php.exe" joomla.php user --id=%usr_id%
+ECHO ON
+php joomla.php user  --id=%usr_id% 
 
-GOTO :EOF
 
-REM ------------------------------------------
-REM Adds given argument to the already known command arguments
-:AddNextArg
-    Set NextArg=%*
-    Set CmdArgs=%CmdArgs% %NextArg%
-    ECHO  '%NextArg%'
-GOTO :EOF
+ECHO: sorry prepared but not working. May be in a further joomla version
+
 
--- cli/j!_user_list.bat
+++ cli/j!_user_list.bat
@@ -1,35 +1,9 @@
 @ECHO OFF
-REM j!_user_list.bat
-REM
-CLS
+REM php cli/joomla.php list user
+REM CLS
 
-REM Path for calling
-set ExePath=e:\wamp64\bin\php\php8.4.5\
-REM ECHO ExePath: "%ExePath%"
-
-if exist "%ExePath%php.exe" (
-    REM path known (WT)
-    ECHO ExePath: "%ExePath%"
-) else (
-    REM Direct call
-    ECHO PHP in path variable
-    set ExePath=
-)
-
-"%ExePath%php.exe" --version
-
-ECHO ----------------------------------------------
+ECHO.
+ECHO --- php joomla.php user:list ---------------------
 ECHO.
 
-echo --- "%ExePath%php.exe" joomla.php user:list %1
-"%ExePath%php.exe" joomla.php user:list %1
-
-GOTO :EOF
-
-REM ------------------------------------------
-REM Adds given argument to the already known command arguments
-:AddNextArg
-    Set NextArg=%*
-    Set CmdArgs=%CmdArgs% %NextArg%
-    ECHO  '%NextArg%'
-GOTO :EOF
+php joomla.php user:list
